a489d7e2afd2f44a832f796515c5c2ca22776611
Yahir Rendon <yahir.rendon@snhu.edu>
2018-04-10 21:48:27 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/happybirthday/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.happybirthday;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        }
+}
